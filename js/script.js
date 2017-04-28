14ca0d5463d3130d342cc91d3dcfa3ef760c3f3e
CHBaker <charlesprobaker@mail.com>
2017-04-27 22:26:41 -0600

    BUG cat levels fail
    cat level does not change after set clicks

--- js/script.js
+++ js/script.js
@@ -3,16 +3,22 @@ var ViewModel = function () {
 	this.name = ko.observable('Marbles');
 	this.imgSrc = ko.observable('images/marbles.jpg');
 	this.levels = ko.observableArray();
-	this.levels.push('Piss-Ant', 'Hopper', 'Jumper', 
-		'Round House Kicker', 'Fireball Thrower', 
-		'Inferno of Death and Laziness');
-	this.levelTrack = ko.observable(0);
 
+	// list of what level the cat is, and how many clicks to achieve
+	this.levels.push([
+		{ level: 'Piss-Ant', click: 0 }, { level: 'Hopper', click: 10 },
+		{ level: 'Jumper', click: 20}, { level: 'Round House Kicker',
+		click: 30 }, { level: 'Fireball Thrower', click: 40 }, 
+		{ level: 'Inferno of Death and Laziness', click: 50}
+	]);
+
+	// change level based on clicks
 	this.level = ko.computed(function () {
-		if (this.clickCount() < 10) {
-			return this.levels()[0];
-		} else {
-			return this.levels()[1];
+		for (var i = 0; i < this.levels().length; i++) {
+			// if clicks == the click in the level
+			if (this.clickCount() == this.levels()[i].click) {
+				return this.levels()[i].level;
+			}
 		}
 	}, this);
 
