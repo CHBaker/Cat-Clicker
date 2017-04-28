5d693c0b016a6bccd8851810e71d8f43f7551329
CHBaker <charlesprobaker@mail.com>
2017-04-27 22:13:18 -0600

    ADD basic level change
    Changes level based on clicks of cat

--- js/script.js
+++ js/script.js
@@ -2,6 +2,19 @@ var ViewModel = function () {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Marbles');
 	this.imgSrc = ko.observable('images/marbles.jpg');
+	this.levels = ko.observableArray();
+	this.levels.push('Piss-Ant', 'Hopper', 'Jumper', 
+		'Round House Kicker', 'Fireball Thrower', 
+		'Inferno of Death and Laziness');
+	this.levelTrack = ko.observable(0);
+
+	this.level = ko.computed(function () {
+		if (this.clickCount() < 10) {
+			return this.levels()[0];
+		} else {
+			return this.levels()[1];
+		}
+	}, this);
 
 	this.increment = function () {
 		this.clickCount(this.clickCount() + 1);
