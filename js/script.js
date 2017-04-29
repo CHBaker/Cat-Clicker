41c697854abcf2d67030885ffbe90a36ada32d25
CHBaker <charlesprobaker@mail.com>
2017-04-29 16:02:23 -0600

    ADD Levels for cat
    Cat Levels increase with clicks

--- js/script.js
+++ js/script.js
@@ -2,22 +2,21 @@ var ViewModel = function () {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Marbles');
 	this.imgSrc = ko.observable('images/marbles.jpg');
-	this.levels = ko.observableArray();
 
 	// list of what level the cat is, and how many clicks to achieve
-	this.levels.push([
-		{ level: 'Piss-Ant', click: 0 }, { level: 'Hopper', click: 10 },
-		{ level: 'Jumper', click: 20}, { level: 'Round House Kicker',
-		click: 30 }, { level: 'Fireball Thrower', click: 40 }, 
-		{ level: 'Inferno of Death and Laziness', click: 50}
-	]);
+	this.levels = [
+		{ level: 'Piss-Ant', click: 10 }, { level: 'Hopper', click: 20 },
+		{ level: 'Jumper', click: 30}, { level: 'Round House Kicker',
+		click: 40 }, { level: 'Fireball Thrower', click: 50 }, 
+		{ level: 'Inferno of Death and Laziness', click: 60}
+	];
 
 	// change level based on clicks
 	this.level = ko.computed(function () {
-		for (var i = 0; i < this.levels().length; i++) {
+		for (var i = 0; i < this.levels.length; i++) {
 			// if clicks == the click in the level
-			if (this.clickCount() == this.levels()[i].click) {
-				return this.levels()[i].level;
+			if (this.clickCount() <= this.levels[i].click) {
+				return "Level :: " + this.levels[i].level;
 			}
 		}
 	}, this);
