25334029de25a86fb98b61cf95b01ba48be6a0eb
CHBaker <charlesprobaker@mail.com>
2017-05-01 00:12:49 -0600

    ADD cat list
    ADD cat list, forEach cat, create new Cat in viewmodel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+var initialCats = [
+	{
+		name: "Lil' Benny", 
+		clickCount: 0,
+		imgSrc: "http://egotvonline.com/wp-content/uploads/2012/12/Cute-lion.jpg",
+		nickNames: ['Ben', 'Ben there', 'B-Dawg']
+	},
+	{
+		name: "Killa' Kyle", 
+		clickCount: 0, 
+		imgSrc: "https://s-media-cache-ak0.pinimg.com/564x/f0/f3/a0/f0f3a07427d528232f5fdd3d2c1e46a5.jpg",
+		nickNames: ['Kylo Ren', 'oKAY', 'Kay']
+	},
+	{
+		name: "Kalifa", 
+		clickCount: 0, 
+		imgSrc: "http://orig06.deviantart.net/c29b/f/2011/063/4/0/cute_baby_hunter_by_woxys-d3awuft.jpg",
+		nickNames: ['Whiz', 'Kali', 'Kali fornia dreamin']
+	},
+	{
+		name: "Bobby B.", 
+		clickCount: 0, 
+		imgSrc: "https://s-media-cache-ak0.pinimg.com/736x/fb/e5/05/fbe5050b6a52a05ffbe9cb7ffb012bae.jpg",
+		nickNames: ['Bob', 'Boba', 'Bee Boi']
+	},
+	{
+		name: "Multigrain", 
+		clickCount: 0, 
+		imgSrc: "http://www.cutestpaw.com/wp-content/uploads/2012/08/Snoopy-049.jpg",
+		nickNames: ['Grain', 'Healthy Choice', 'Bread Boi']
+	},
+	{
+		name: "Marbles", 
+		clickCount: 0, 
+		imgSrc: "images/marbles.jpg",
+		nickNames: ['Marbs', 'Mr. Marbles', 'Lay-Z Boi']
+	}
+];
+
+
 var Cat = function (data) {
 	this.clickCount = ko.observable(data.clickCount);
 	this.name = ko.observable(data.name);
@@ -27,17 +67,17 @@ var Cat = function (data) {
 
 var ViewModel = function () {
 
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function (catItem) {
+		self.catList.push( new Cat (catItem) );
+	})
+
+	this.currentCat = ko.observable( this.catList()[0] );
+
 	// could use self = this to access viewmodel instead of binding context
-
-	this.currentCat = ko.observable( new Cat({
-		clickCount: 0, 
-		name: 'Marbles',
-		imgSrc: 'images/marbles.jpg',
-		nickNames: ['Marbs',
-				   'Mr. Marbles',
-				   'Lay-Z Boi']
-	}) );
-
 	this.increment = function () {
 		// self.currentCat().clickCount(...) 
 		// accesses viewmodel instead of binding context
